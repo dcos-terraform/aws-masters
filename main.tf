229f6ed05cac8f4a9878ee356bfcfa88287eeabd
Jan Ulferts <julferts@mesosphere.com>
2018-08-10 15:13:03 +0200

    replace null resource name

--- main.tf
+++ main.tf
@@ -57,7 +57,7 @@ module "dcos-master-instances" {
   tags               = "${var.tags}"
 }
 
-resource "null_resource" "masters" {
+resource "null_resource" "masters-prereq" {
   // if the user supplies an AMI or user_data we expect the prerequisites are met.
   count = "${coalesce(var.aws_ami, var.user_data) == "" ? var.num_masters : 0}"
 
